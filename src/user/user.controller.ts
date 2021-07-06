0560eb50a0396e5de6e1533106dc42e6e4cf4fda
fencim <fencim@gmail.com>
2021-07-06 10:29:13 +0800

    create user > controller + service + dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { create } from 'domain';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UserDto } from 'src/dto/user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private userService:UserService) {
-        
+    constructor(private userService:UserService) { }
+    
+    @Post()
+    async create(@Body() user: CreateUserDto) :Promise<UserDto> {
+        return this.userService.create(user);
     }
 }
